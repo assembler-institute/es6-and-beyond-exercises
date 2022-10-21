38f8f169ffa6fcd362325a16f5dc180b41130e2d
juanfco86 <juanfco86@gmail.com>
2022-10-21 16:17:43 +0200

    JS ES6 exercises completed

--- src/08-exercise/08-exercise.js
+++ src/08-exercise/08-exercise.js
@@ -11,9 +11,8 @@
 
 // Finish the code of the function
 function exercise08() {
-    function func(a, b) {
-        return a + b;
-    }
+    const func = (a, b) => a + b;
+    
 
     // Don’t change the code bellow this line
     return func;
